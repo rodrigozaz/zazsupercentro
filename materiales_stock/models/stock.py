69587ba68783ad38212b8e5ae0b7a3273d49f569
sna-odoo <sna@odoo.com>
2020-07-10 16:40:15 -0700

    [ADD] Error on quantity

--- materiales_stock/models/stock.py
+++ materiales_stock/models/stock.py
@@ -4,11 +4,16 @@ from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
-class StockMoveLine(models.Model):
-    _inherit = "stock.move.line"
+class Picking(models.Model):
+    _inherit = "stock.picking"
 
-    @api.constrains('qty_done')
-    def _validate_quantity(self):
+    def button_validate(self):
+        res = super(Picking,self).button_validate()
         for record in self:
-            if record.qty_done > record.move_id.product_uom_qty:
-                raise ValidationError(_('You cannot receive more than the ordered quantity. Please, enter another quantity'))
+            if record.picking_type_id.name == 'Receipts':
+                move_lines = record.move_lines or record.move_ids_without_package
+                for line in move_lines:
+                    if line.quantity_done > line.product_uom_qty:
+                        raise ValidationError('You cannot receive more than the ordered quantity. Please, enter another quantity')
+        return res 
+        

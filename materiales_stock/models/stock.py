8fe090b1c59091c40c4d675c8b373d7f8999997f
sna-odoo <sna@odoo.com>
2020-07-10 17:06:02 -0700

    [ADD] Error on quantity

--- materiales_stock/models/stock.py
+++ materiales_stock/models/stock.py
@@ -8,12 +8,12 @@ class Picking(models.Model):
     _inherit = "stock.picking"
 
     def button_validate(self):
+        self.ensure_one()
+        if self.picking_type_code == 'incoming':
+            move_lines = self.move_lines or self.move_ids_without_package
+            for line in move_lines:
+                if line.quantity_done > line.product_uom_qty:
+                    raise ValidationError('You cannot receive more than the ordered quantity. Please, enter another quantity')
         res = super(Picking,self).button_validate()
-        for record in self:
-            if record.picking_type_id.name == 'Receipts':
-                move_lines = record.move_lines or record.move_ids_without_package
-                for line in move_lines:
-                    if line.quantity_done > line.product_uom_qty:
-                        raise ValidationError('You cannot receive more than the ordered quantity. Please, enter another quantity')
         return res 
         

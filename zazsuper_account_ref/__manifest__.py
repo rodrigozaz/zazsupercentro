7fe24b87c14a622ec968a584aeaa29ccea0ddabd
Zeno Nanon <zna@odoo.com>
2020-07-28 16:21:03 -0700

    [ADD] module to add reference field to aged payable report

---
+++ zazsuper_account_ref/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "MC:Add Reference field in Aged Payable",
+
+    'summary': """
+        Add reference field to aged payable report""",
+
+    'description': """
+        Task ID:2283600
+    """,
+
+    'author': "Odoo Inc",
+    'website': "http://www.odoo.com",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'license': 'OEEL-3',
+    'depends': ['account_reports'],
+}

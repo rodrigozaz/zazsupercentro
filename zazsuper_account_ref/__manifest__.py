0a1e207167344a600ed534f9fe370a8a2e5ee976
aal-odoo <60622764+aal-odoo@users.noreply.github.com>
2020-08-27 12:57:50 -0700

    Update __manifest__.py

--- zazsuper_account_ref/__manifest__.py
+++ zazsuper_account_ref/__manifest__.py
@@ -9,9 +9,9 @@
         Task ID:2283600
     """,
 
-    'author': "Odoo Inc",
-    'website': "http://www.odoo.com",
-    'category': 'Custom Development'
+    'author': 'Odoo Inc',
+    'website': 'http://www.odoo.com',
+    'category': 'Custom Development',
     'version': '1.0',
     'license': 'OEEL-1',
     'depends': ['account_reports'],

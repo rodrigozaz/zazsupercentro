c05b3aa78bf7120e6a48291b322ae0ec915022cb
Zeno Nanon <zna@odoo.com>
2021-02-26 15:22:48 -0800

    [FIX] Auto compute user link in res.partner

--- materiales_sale_discount/models/res_partner.py
+++ materiales_sale_discount/models/res_partner.py
@@ -10,7 +10,7 @@ class Partner(models.Model):
     self_user_id = fields.Many2one('res.users', string='User Record', readonly=True, compute='_compute_self_user', store=True)
     groups_id = fields.Many2many('res.groups', string='Access Groups', related='self_user_id.groups_id')
 
+    @api.depends('name')
     def _compute_self_user(self):
         for partner in self:
             partner.self_user_id = self.env['res.users'].sudo().search([('partner_id', '=', partner.id)])
-            

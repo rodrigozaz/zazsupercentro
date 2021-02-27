be554a8c7074e2b958baf7c810d55bb5b5ac8d9d
Zeno Nanon <zna@odoo.com>
2021-02-26 16:25:54 -0800

    [FIX] Change visibility parameters with validate and mark to do

--- materiales_stock_interwarehouse/models/stock_picking.py
+++ materiales_stock_interwarehouse/models/stock_picking.py
@@ -4,6 +4,11 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
@@ -18,7 +23,7 @@ class StockPicking(models.Model):
     def button_request(self):
         for picking in self:
             template_id = self.env.ref('materiales_stock_interwarehouse.transfer_request_email')
-            admins = picking.env['res.users'].search([('location_ids', 'in', [picking.location_id.id])])
+            admins = picking.env['res.users'].search(['&',('location_ids', 'in', [picking.location_id.id]),('groups_id','=', 20)])
             if admins:
                 for admin in admins:
                     email = '"{}" <{}>'.format(admin.name, admin.login)
@@ -34,7 +39,9 @@ class StockPicking(models.Model):
         for picking in self:
             if not (picking.immediate_transfer) and picking.state == 'draft':
                 picking.show_validate = False
-            elif picking.picking_type_id.sequence_code == 'INT' and (picking.state == 'draft' or (picking.state == 'to_approve' and picking.location_id.id not in self.env.user.location_ids.ids)):
+            elif picking.picking_type_id.sequence_code == 'INT' and picking.state == 'to_approve' and picking.location_id.id in self.env.user.location_ids.ids and 20 in self.env.user.groups_id.ids:
+                picking.show_validate = True
+            elif picking.picking_type_id.sequence_code == 'INT' and picking.state in ['draft', 'assigned', 'waiting', 'to_approve']:
                 picking.show_validate = False
             elif picking.state not in ('draft', 'waiting', 'confirmed', 'assigned', 'to_approve') or not picking.is_locked:
                 picking.show_validate = False
@@ -44,7 +51,7 @@ class StockPicking(models.Model):
     @api.depends('state', 'location_dest_id')
     def _compute_show_request(self):
         for picking in self:
-            if picking.state in ['draft', 'waiting'] and picking.location_dest_id.id in self.env.user.location_ids.ids and picking.picking_type_id.sequence_code == 'INT':
+            if picking.state in ['draft', 'waiting', 'assigned'] and picking.location_dest_id.id in self.env.user.location_ids.ids and picking.picking_type_id.sequence_code == 'INT':
                 picking.show_request = True
             else:
                 picking.show_request = False
